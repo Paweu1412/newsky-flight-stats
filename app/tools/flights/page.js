5dc64bf0983ce6f2ccee89c048c853c0b0bb1a9f
Paweł Nosalski <fordcrown2009@gmail.com>
2024-02-03 21:29:33 +0100

    Frontend basics

---
+++ app/tools/flights/page.js
@@ -0,0 +1,93 @@
+"use client";
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+import { Button, Stack } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { useState } from 'react';
+
+import { toTimestamp } from '../../utils/utils';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+];
+
+export default function Dashboard() {
+  const [fromDate, setFromDate] = useState(null);
+  const [toDate, setToDate] = useState(null);
+
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+
+  return (
+    <div className="Dashboard flex justify-center">
+      <div className="container w-[900px] flex flex-col items-center">
+        <div className="date-placeholders pt-5">
+          <Stack spacing={2} direction="row">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>
+                <DatePicker onChange={(value) => {console.log(value); setFromDate(toTimestamp(value))}} label="From" />
+              </DemoContainer>
+            </LocalizationProvider>
+          
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DatePicker']}>
+                <DatePicker onChange={(value) => {console.log(value); setToDate(toTimestamp(value))}} label="To" />
+              </DemoContainer>
+            </LocalizationProvider>
+          </Stack>
+          
+          <Button variant="contained" className="mt-5 w-[100%] bg-second" onClick={() => console.log(fromDate, toDate)}>CHECK FLIGHTS</Button>
+        </div>
+
+        <div className="flights-table mt-5">
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 5,
+                },
+              },
+            }}
+            pageSizeOptions={[5]}
+            checkboxSelection
+            disableRowSelectionOnClick
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
