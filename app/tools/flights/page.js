b235fafffd19cc8918b386cb1b8cbc27cf98329b
Paweł Nosalski <fordcrown2009@gmail.com>
2024-02-04 19:46:10 +0100

    Basic functions

--- app/tools/flights/page.js
+++ app/tools/flights/page.js
@@ -1,91 +1,115 @@
 "use client";
 
+import { useState } from 'react';
+import { Button, Stack } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-import { Button, Stack } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { useState } from 'react';
-
-import { toTimestamp } from '../../utils/utils';
-
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
+  { field: 'pilot', headerName: 'Pilot', width: 150, editable: false, 
+    renderCell: (params) => (
+      <div className="flex items-center">
+        <img 
+          src={`https://newsky.app/api/pilot/avatar/${params.row.pilot.avatar}`}
+          alt={params.row.pilot.avatar} 
+          className="rounded-full mr-2 w-8 h-8"
+        />
+        {params.row.pilot.fullname.split(' ')[0]}
+      </div>
+    ), 
   },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
+  { field: 'dep.icao', headerName: 'Departure', width: 100, editable: false, valueGetter: (params) => params.row.dep.icao },
+  { field: 'arr.icao', headerName: 'Arrival', width: 100, editable: false, valueGetter: (params) => params.row.arr.icao },
+  { field: 'aircraft.airframe.name', headerName: 'Aircraft', width: 150, editable: false, valueGetter: (params) => params.row.aircraft.airframe.name },
+  { field: 'result.totals.distance', headerName: 'Distance', width: 100, editable: false, valueGetter: (params) => params.row.result.totals.distance + ' nm'},
+  { field: 'result.totals.time', headerName: 'Duration', width: 100, editable: false, valueGetter: (params) => params.row.result.totals.time + ' min' },
+  { field: 'simulator', headerName: 'Simulator', width: 100, editable: false, valueGetter: (params) => params.row.simulator.toUpperCase() },
+  { field: 'network.name', headerName: 'Network', width: 100, editable: false, valueGetter: (params) => (params.row.network?.name || '-').toUpperCase() },
 ];
 
 export default function Dashboard() {
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
+  const [flights, setFlights] = useState([]);
 
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+  const fetchFlights = async () => {
+    const url = 'https://newsky.app/api/airline-api/flights/bydate';
+    const token = process.env.NEXT_PUBLIC_TOKEN;
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ start: fromDate.toISOString(), end: toDate.toISOString() }),
+      });
+  
+      if (response.ok) {
+        let data = await response.json();
+        data = data.results.map((flight, index) => ({ ...flight, id: index }));
+  
+        setFlights(data);
+      } else {
+        console.error('Failed to fetch flights');
+      }
+    } catch (error) {
+      console.error('Error fetching flights', error);
+    }
+  };  
+
+  const handleCheckFlights = () => {
+    fetchFlights();
+  };
 
   return (
     <div className="Dashboard flex justify-center">
-      <div className="container w-[900px] flex flex-col items-center">
+      <div className="container w-max flex flex-col items-center">
         <div className="date-placeholders pt-5">
           <Stack spacing={2} direction="row">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DemoContainer components={['DatePicker']}>
-                <DatePicker onChange={(value) => {setFromDate(value)}} label="From" />
+                <DatePicker onChange={(value) => setFromDate(value)} label="From" />
               </DemoContainer>
             </LocalizationProvider>
-          
+
             <LocalizationProvider dateAdapter={AdapterDayjs}>
               <DemoContainer components={['DatePicker']}>
-                <DatePicker onChange={(value) => {setToDate(value)}} label="To" />
+                <DatePicker onChange={(value) => setToDate(value)} label="To" />
               </DemoContainer>
             </LocalizationProvider>
           </Stack>
-          
-          <Button variant="contained" className="mt-5 w-[100%] bg-second" onClick={() => console.log(fromDate, toDate)}>CHECK FLIGHTS</Button>
+
+          <label>
+          <input
+            type="checkbox"
+            // checked={onlyVatsim}
+            // onChange={() => setOnlyVatsim(!onlyVatsim)}
+          />
+          Only VATSIM
+        </label>
+
+          <Button variant="contained" className="mt-5 w-[100%] bg-second" onClick={handleCheckFlights}>
+            CHECK FLIGHTS
+          </Button>
         </div>
 
-        <div className="flights-table mt-5">
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 5,
+        <div className="flights-table w-max mt-5">
+         {flights.length > 0 &&
+            <DataGrid
+              rows={flights}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: { pageSize: 25, page: 0 },
                 },
-              },
-            }}
-            pageSizeOptions={[5]}
-            checkboxSelection
-            disableRowSelectionOnClick
-          />
+              }}
+            />
+          }
         </div>
       </div>
     </div>
